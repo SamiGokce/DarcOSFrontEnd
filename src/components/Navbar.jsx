3b1ddc9c8ae9525cb7482dcfcb7a3354361efab5
Sami Gokce <samigokce@eduroam-campus-10-36-123-166.campus-dynamic.uwaterloo.ca>
2025-11-03 13:53:34 -0500

    Initial commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import { Link } from "react-router-dom";
+
+function Navbar() {
+  return (
+    <nav style={{ background: "black", padding: "1rem", display: "cover", justifyContent: "space-between" }}>
+      <h1 style={{ color: "white" }}>DarcOS</h1>
+      <div>
+        <Link to="/" style={{ color: "white", margin: "0 1rem" }}>Home</Link>
+        <Link to="/projects" style={{ color: "white", margin: "0 1rem" }}>Projects</Link>
+        <Link to="/about" style={{ color: "white", margin: "0 1rem" }}>About</Link>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,3 @@
+export default function Home() {
+  return <h2>Welcome to DarcOS Home Page</h2>;
+}
